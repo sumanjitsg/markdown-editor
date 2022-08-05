1c7d833c46442781acf20c00e0289e70f8f76e30
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2022-08-05 20:20:58 +0530

    remove web vitals

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,8 +9,6 @@ import Splitter from "components/Workspace/Splitter";
 import { Provider } from "react-redux";
 import { store } from "store";
 
-import reportWebVitals from "./reportWebVitals";
-
 // Styles
 import "./styles/index.scss";
 
@@ -34,8 +32,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
