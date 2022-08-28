8263e1e977e51b921c3f9a52f211a7d237e34ebd
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2022-08-28 19:03:43 +0530

    save document behavior added

--- src/components/Header/SaveButton.tsx
+++ src/components/Header/SaveButton.tsx
@@ -1,12 +1,16 @@
 import { useAppSelector } from "store/hooks";
-import { selectActiveDocumentId } from "store/documentsSlice";
+import {
+  selectActiveDocumentContent,
+  selectActiveDocumentMetadata,
+} from "store/documentsSlice";
 
 import { ReactComponent as IconSave } from "assets/icon-save.svg";
 
 import styles from "styles/components/Header/IconSave.module.scss";
 
 function SaveButton() {
-  const activeId = useAppSelector(selectActiveDocumentId);
+  const { activeId, content } = useAppSelector(selectActiveDocumentContent);
+  const { metadata } = useAppSelector(selectActiveDocumentMetadata);
 
   return (
     <button
@@ -14,11 +18,11 @@ function SaveButton() {
       aria-label="Download active document"
       className={`w-10 h-10 flex items-center justify-center rounded bg-orange-400 hover:bg-orange-300 focus:bg-orange-300 disabled:bg-gray-600`}
       onClick={() => {
-        const text = "# test text";
+        const text = content ?? "";
         // text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
         const blob = new Blob([text], { type: "text/plain" });
         const anchor = document.createElement("a");
-        anchor.download = "test.md";
+        anchor.download = metadata?.documentName ?? "untitled.md";
         anchor.href = window.URL.createObjectURL(blob);
         anchor.target = "_blank";
         anchor.style.display = "none"; // just to be safe!
