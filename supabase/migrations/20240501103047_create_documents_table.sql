4c4276dc9e8ffce99c0666707f195e34bc2b4794
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2026-03-25 21:07:39 +0530

    feat: remove supabase support

--- supabase/migrations/20240501103047_create_documents_table.sql
+++
@@ -1,7 +0,0 @@
-create table documents (
-  id serial primary key,
-  document_name text unique not null,
-  created_on date default current_date,
-  last_modified date default current_date,
-  document_content text
-  );
